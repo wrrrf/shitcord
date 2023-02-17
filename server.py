7b282c65c7b731a4239103b338653be850c01ca1
poopface-exploded <112532586+poopface-exploded@users.noreply.github.com>
2023-02-17 14:58:18 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import queue
+import datetime
 
 def run_server():
     PORT = 5051
@@ -10,7 +11,9 @@ def run_server():
     HEADER = 64
 
     messages = queue.Queue()
+    message_log = []
     clients = []
+    usernames = {}
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -30,8 +33,11 @@ def run_server():
         #print(addr)
         #print(conn)
         #conn.send("[MESSAGE RECEIVED]".encode(FORMAT))
+            #if message == '!LOGS':
+                #print(message_log)
             try:
                 messages.put((message, addr, conn))
+                message_log.append({addr, message})
             except:
                 print("failed")
 
@@ -42,11 +48,15 @@ def run_server():
                 #print(message)
                 if conn not in clients:
                     clients.append(conn)
-                    conn.send('[CONNECTION SUCCESSFUL]'.encode(FORMAT))
+                    usernames.update({conn: message})
                 for client in clients:
                 #print(client)
                     #print (addr)
-                    client.send(f"<{str(addr)}> {message}".encode(FORMAT))
+                    try:
+                        user = usernames[conn]
+                        client.send(f"<{str(user)}> {message}".encode(FORMAT))
+                    except:
+                        clients.pop(client)
                     #print(message)
 
 #def client():
@@ -56,6 +66,10 @@ def run_server():
         server.listen()
         while True:
             conn, addr = server.accept()
+            for message in message_log:
+                conn.send(str(message).encode(FORMAT))
+            conn.send('[CONNECTED]'.encode(FORMAT))
+            conn.send('[! THE FIRST MESSAGE YOU SEND WILL BE YOUR USERNAME !]'.encode(FORMAT))
             thread = threading.Thread(target = receive, args = (conn, addr))
             thread.start()
             thread_brdcst = threading.Thread(target = broadcast)
