a2a158378635a71b677e7fd928e5c0f6f919411b
poopface-exploded <112532586+poopface-exploded@users.noreply.github.com>
2023-02-17 14:57:46 -0800

    Update client.py

--- client.py
+++ client.py
@@ -1,38 +1,60 @@
 import socket
 import threading
+from flask import Flask, render_template, request
+import queue
 
-def run_client():
-    HEADER = 64
-    PORT = 5051
-    FORMAT = 'utf-8'
-    DISCONNECT_MESSAGE = "!DISCONNECT"
-    SERVER = '192.168.1.76'
-    ADDR = (SERVER, PORT)
+HEADER = 64
+CLIENT_PORT = 5051
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+CLIENT_SERVER = socket.gethostbyname(socket.gethostname())
+CLIENT_ADDR = (CLIENT_SERVER, CLIENT_PORT)
+
+client = ()
+
+received_messages = []
+message = ()
+
+def client_start():
+
+    #old_message = ()
+
+    global client
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
+    client.connect(CLIENT_ADDR)
+
+    while True:
+        message = (client.recv(2048).decode(FORMAT))
+        try:
+            received_messages.append((message))
+            print (message)
+        except:
+            pass
+
+def client_send(input_msg):
+
+    global FORMAT
+    global HEADER
+    global client
 
     def send(msg):
-        message = msg.encode(FORMAT)
+        message = str(msg).encode(FORMAT)
         msg_length = len(message)
         send_length = str(msg_length).encode(FORMAT)
         send_length += b' ' * (HEADER - len(send_length))
         client.send(send_length)
         client.send(message)
 
-    def receive():
-        while True:
-            print(client.recv(2048).decode(FORMAT))
-
-    thread = threading.Thread(target = receive)
-    thread.start()
-
     def enter():
-        while True:
-            message = (input(""))
-            send(message)
-            if message == DISCONNECT_MESSAGE:
-                break
 
-    thread_2 = threading.Thread(target = enter)
-    thread_2.start()
+        global DISCONNECT_MESSAGE
+
+        message = input_msg
+        print(message)
+        send(message)
+        if message == DISCONNECT_MESSAGE:
+            exit()
+
+
+    enter()
