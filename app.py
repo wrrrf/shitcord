89c932e328a69d68c27d25bd6b45246d890c9ae0
poopface-exploded <112532586+poopface-exploded@users.noreply.github.com>
2023-02-17 14:57:19 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,50 @@
 import threading
-from client import run_client
 from server import run_server
-from flask import Flask
+from client import client_start, client_send, received_messages
+from flask import Flask, render_template, request, send_from_directory
+import pandas as pd
+from turbo_flask import Turbo
+import time
+
+app = Flask(__name__)
 
 def server():
     run_server()
 
-def client():
-    run_client()
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "<p>Hello, World!</p>"
+messages_copy = ()
 
 server_start = threading.Thread(target = server)
 server_start.start()
 
-client_start = threading.Thread(target = client)
-client_start.start()
+form_message = ()
+
+@app.route('/', methods = ['GET', 'POST'])
+def messages():
+    return render_template('index.html', message_recv = received_messages)
+
+@app.route('/create_post', methods = ['POST'])
+def create_file():
+    if request.method == 'POST':
+        return render_template('messages.html', message_recv = received_messages)
+
+@app.route('/submit_message', methods = ['GET', 'POST'])
+def submit_message():
+    submitted_message = (request.form.get('chatbox'))
+    print(submitted_message)
+    client_send(submitted_message)
+    return ('', 204)
+
+
+start_client = threading.Thread(target = client_start)
+start_client.start()
+
+#client_send = threading.Thread(target = client_send)
+#client_send.start()
+
+if __name__ == '__main__':
+    app.run(
+        host = '127.0.0.1',
+        port = 5051,
+        debug = False
+        )
+
