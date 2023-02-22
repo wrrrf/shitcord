5f8064a6c78efe99cabf422f3af5de57ef1802ba
poopface-exploded <112532586+poopface-exploded@users.noreply.github.com>
2023-02-21 18:13:49 -0800

    Update client.py

--- client.py
+++ client.py
@@ -25,7 +25,7 @@ def client_start():
     while True:
         message = (client.recv(2048).decode(FORMAT))
         received_messages.append(message)
-        print ('message received!: ' + (message.decode(FORMAT))
+        print ('message received!: ' + (message.decode(FORMAT)))
 
 def client_send(input_msg):
 
