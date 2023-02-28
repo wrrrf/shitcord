953ddfc3378dcbcf760542996c55fc3e82ce48dc
poopface-exploded <112532586+poopface-exploded@users.noreply.github.com>
2023-02-27 16:41:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def messages():
 @app.route('/create_post', methods = ['POST'])
 def create_file():
     if request.method == 'POST':
+        print(received_messages)
         return render_template('messages.html', message_recv = received_messages)
 
 @app.route('/submit_message', methods = ['GET', 'POST'])
