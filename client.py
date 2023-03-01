70f177e449aee9268ed7e6436123be90e8e8d767
poopface-exploded <112532586+poopface-exploded@users.noreply.github.com>
2023-02-28 18:47:55 -0800

    Update client.py

--- client.py
+++ client.py
@@ -41,7 +41,7 @@ def client_send(input_msg, id):
         msg_length = len(message)
         send_length = str(msg_length).encode(FORMAT)
         send_length += b' ' * (HEADER - len(send_length))
-        print(send_length)
+        print(str(send_length))
         client.send(send_length)
         client.send(message)
         print('message sent!: ' + (message.decode(FORMAT) + f' id = {id}'))
