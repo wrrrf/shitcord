aeedab39828dbdb6896b53f4d0b2ddd9f4057fbe
poopface-exploded <112532586+poopface-exploded@users.noreply.github.com>
2023-03-01 19:58:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import threading
 from server import run_server
-from client import client_start, client_send, received_messages, client_id
+from client import client_start, client_send, received_messages, client_id, client
 from flask import Flask, render_template, request, send_from_directory
 import time
 
@@ -17,14 +17,24 @@ server_start.start()
 form_message = ()
 current_id = ()
 
+connected_clients = []
+
 @app.route('/', methods = ['GET', 'POST'])
 def messages():
     global client_id
+    global client
+    global connected_clients
+    
     client_id_copy = client_id
+    client_copy = client
     client_id += 1
     print(client_id)
     start_client = threading.Thread(target = client_start, args = [client_id_copy])
     start_client.start()
+    connected_clients.append({
+        "id": client_id_copy,
+        "conn": client
+    })
     return render_template('index.html', message_recv = received_messages, client_id = client_id_copy)
 
 @app.route('/create_post', methods = ['POST'])
@@ -35,11 +45,15 @@ def create_file():
 
 @app.route('/submit_message', methods = ['GET', 'POST'])
 def submit_message():
+    
+    global connected_clients
+    
     client_id = (request.form.get('client_id'))
     submitted_message = (request.form.get('text'))
+    client_addr = connected_clients[int(client_id)]['conn']
     if submitted_message != (None):
         print(submitted_message)
-        client_send(submitted_message, client_id)
+        client_send(client_addr, submitted_message, client_id)
         print(received_messages)
     else:
         print('none')
