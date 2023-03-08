58b93c9c65b241ba1be45413e1704799f5727bbd
poopface-exploded <112532586+poopface-exploded@users.noreply.github.com>
2023-03-07 17:27:14 -0800

    Update client.py

--- client.py
+++ client.py
@@ -23,7 +23,7 @@ def client_start(id):
     
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(CLIENT_ADDR)
-    print(client)
+    print(f'{client} FROM CLIENT.PY')
 
     while True:
         message = (client.recv(2048).decode(FORMAT))
